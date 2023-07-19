cc62208bbf33f11eaea29820bd5d753af7e9378d
ryanhamilton-source <ryanhamiltonbusiness@gmail.com>
2023-07-19 22:29:19 +1000

    updated about me
    tech update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,11 +11,45 @@ const About = () => {
             </div>
 
             <p className='text-xl mt-20'>
-                Paragraph.ferlknrgjgnerjerngknerjgrnngrnrfjrfnjfjffrjn
+            Motivated and detail-oriented student pursuing a Bachelor of Business (Finance) 
+            and Bachelor of Science in Information Technology (Enterprise Systems) seeking 
+            to leverage academic knowledge and diverse work experience in a challenging role within the
+             finance or IT/Software Developer Industry. 
             </p>
 
             <p className='text-xl'>
-                whebwefbewjbefkdenwkjwnendjdwenedejn testing
+            EXPERIENCE
+2019  CURRENT
+BAR AND GAMING SUPIVISOR, HARBORD BOWLING CLUB
+•	Ensured smooth operation of the bar while maintaining high customer satisfaction.
+•	Provided prompt and effective responses to customer requests and queries.
+•	Demonstrated adaptability in overcoming obstacles in high-pressure situations.
+•	Maintained cash floats and tills in accordance with company policy.
+JUNE 2022  CURRENT
+JUNIOR DEVELOPER, STREAMLINE ID
+•	Conducted testing and reporting of code for onboarding systems in the financial sector.
+•	Contributed to software improvement through problem-solving and feature enhancement.
+•	Assisted the team in supporting a sophisticated cloud-based job management system.
+•	Collaborated with experienced developers in a professional office environment.
+EDUCATION
+2018 
+HSC, ST AUGUSTINES COLLEGE BROOKVALE
+
+2020  NOVEMBER 2023
+BACHELOR OF BUSINESS (FINANCE), UNIVERSITY OF TECHNOLOGY SYDNEY
+•	Focused on financial markets, investment strategies, corporate finance, and financial analysis.
+•	Developed a strong understanding of financial theories, principles, and practices.
+•	Gained experience in financial modeling, risk management, and decision-making.
+
+
+
+
+2020  NOVEMBER 2023
+BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY (ENTERPRISE SYSTEMS), UNIVERSITY OF TECHNOLOGY SYDNEY
+•	Concentrated on the design, implementation, and management of large-scale information systems.
+•	Acquired skills in systems analysis, software development, database management, and IT project management.
+•	Gained proficiency in multiple programming languages and software tools.
+
             </p>
         </div>
     </div>
